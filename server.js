060724c7bfb1878c8e77abe687a61161b96f9f97
Oluwagbenga <adulojugbenga@yahoo.com>
2021-08-08 18:37:58 +0100

    CORS edit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.header("Access-Control-Allow-Origin", "https://myresume-api.herokuapp.com/sendMail");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
